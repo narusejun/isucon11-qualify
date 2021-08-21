158441fdd6b6b3349c906c72124929b4c393ee04
takashi <30363887+wtks@users.noreply.github.com>
2021-08-21 11:25:14 +0900

    s2, s3に設定をコピー

--- s2/deploy.sh
+++ s2/deploy.sh
@@ -1,23 +1,28 @@
 #!/bin/bash -eux
 
-#sudo cp -f etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
-#sudo cp -rf etc/nginx /etc/nginx
+sudo cp -f etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo cp -f etc/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp -f etc/nginx/sites-available/isucondition.conf /etc/nginx/sites-available/isucondition.conf
+
 #cp -f env /home/isucon/env
-#
-#cd /home/isucon/webapp/golang
-#make build
-#
-#sudo systemctl restart mysql
-#sudo systemctl reload nginx
-#sudo systemctl restart web-golang
+
+cd /home/isucon/webapp/go
+go build -o isucondition
+
+sudo systemctl restart mariadb
+sudo systemctl reload nginx
+sudo systemctl restart isucondition.go
 
 
 # slow query logを有効化する
-# QUERY="
-# set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
-# set global long_query_time = 0;
-# set global slow_query_log = ON;
-# "
-#
-# echo $QUERY | sudo mysql -uroot
-# sudo chmod 777 /var/log/mysql/mysql-slow.log
+QUERY="
+ set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
+ set global long_query_time = 0;
+ set global slow_query_log = ON;
+"
+
+echo $QUERY | sudo mysql -uroot
+
+# log permission
+sudo chmod 777 /var/log/nginx /var/log/nginx/*
+sudo chmod 777 /var/log/mysql /var/log/mysql/*
