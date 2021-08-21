4132537145c01a54b86207c9e130d3d94b8dc895
Kazuki Sawada <kazuki@6715.jp>
2021-08-21 10:40:48 +0900

    index

--- app/webapp/sql/0_Schema.sql
+++ app/webapp/sql/0_Schema.sql
@@ -35,3 +35,5 @@ CREATE TABLE `isu_association_config` (
   `name` VARCHAR(255) PRIMARY KEY,
   `url` VARCHAR(255) NOT NULL UNIQUE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+ALTER TABLE `isucondition`.`isu_condition` ADD INDEX `uuid_ts` (`jia_isu_uuid`, `timestamp`);
